44455b1bf4cc4e77eb0fbf058614b8c18d1042b3
Akusko Danil Dmitrievich <akusko.dan@yandex.ru>
2026-01-23 23:35:49 +0700

    Add TooLongTextAnalyzer class

---
+++ src/main/java/org/stepik/TooLongTextAnalyzer.java
@@ -0,0 +1,17 @@
+package org.stepik;
+
+public class TooLongTextAnalyzer implements TextAnalyzer {
+    private final int maxLength;
+
+    public TooLongTextAnalyzer(int maxLength) {
+        this.maxLength = maxLength;
+    }
+
+    @Override
+    public Label processText(String text) {
+        if (text.length() > this.maxLength) {
+            return Label.TOO_LONG;
+        }
+        return Label.OK;
+    }
+}
