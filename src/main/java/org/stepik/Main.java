72651b929ca6d799d5bdbea5fcd68786c811b8fc
Akusko Danil Dmitrievich <akusko.dan@yandex.ru>
2026-01-24 16:55:38 +0700

    Edit main method signature

--- src/main/java/org/stepik/Main.java
+++ src/main/java/org/stepik/Main.java
@@ -1,7 +1,7 @@
 package org.stepik;
 
 public class Main {
-    static void main() {
+    public static void main(String[] args) {
         String[] spamKeywords = new String[] {
                 "Без процентов",
                 "Кредит",
